ff54d58afc9c948952170695c734d0548ab73087
Pratham Shetty <shettypratham9@gmail.com>
2025-05-25 14:18:18 -0400

    Initial Commit

---
+++ Planner/PlannerApp.swift
@@ -0,0 +1,17 @@
+//
+//  PlannerApp.swift
+//  Planner
+//
+//  Created by Pratham Shetty on 25/05/25.
+//
+
+import SwiftUI
+
+@main
+struct PlannerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ PlannerTests/PlannerTests.swift
@@ -0,0 +1,17 @@
+//
+//  PlannerTests.swift
+//  PlannerTests
+//
+//  Created by Pratham Shetty on 25/05/25.
+//
+
+import Testing
+@testable import Planner
+
+struct PlannerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
